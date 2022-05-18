5afaa652bfc655c7eb2c9d300b660447ba3e9074
JaydenDev <willsjayden2@gmail.com>
2022-05-17 21:30:59 -0500

    New additions

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["**/*.html"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+    variants: {
+        backgroundColor: ["dark"],
+    },
+    darkMode: "class",
+}
