57ba3897a6eacf71315569482c5332ab9c9d7d73
JaydenDev <willsjayden2@gmail.com>
2022-05-20 16:14:08 -0500

    changes 5/20/2022

--- library.js
+++ library.js
@@ -2,9 +2,10 @@
 // MIT License
 // BracketsJS
 // Javascript library to make the daily javascript tasks, simpler.
+var brackets = {};
 
 // functions
-function createElem(element, parentID, id, className, innerHTML, href) {
+brackets.createElem = (element, parentID, id, className, innerHTML, href) => {
     var newElement = document.createElement(element);
     if (id) {
         newElement.id = id;
@@ -25,37 +26,48 @@ function createElem(element, parentID, id, className, innerHTML, href) {
     return newElement;
 }
 
-function removeLinks() {
-    var links = document.getElementsByTagName("link");
-    for (var i = 0; i < links.length; i++) {
+brackets.removeLinks = () => {
+        var links = document.getElementsByTagName("link");
+        for (var i = 0; i < links.length; i++) {
         links[i].parentNode.removeChild(links[i]);
     }
 }
-
-function swapStyles(style) {
-    removeLinks();
-    loadCSSFile(style);
+brackets.swapStyles = (style) => {
+    if (!style.endsWith(".css")) {
+        console.log("BracketsJS: " + style + " is not a css file.");
+        return;
+    } else {
+        removeLinks();
+        loadCSSFile(style);
+    }
 }
 
-function addScript(path) {
-    const script = document.createElement('script');
-    script.src = path;
-    document.getElementsByTagName('body')[0].appendChild(script);
+brackets.addScript = (path) =>{
+    if (!path.endsWith(".js")) {
+        console.log('BracketsJS: ' + path + " is not a javascript file.");
+        return;
+    } else {
+        const script = document.createElement('script');
+        script.src = path;
+        document.getElementsByTagName('body')[0].appendChild(script);
+    }
+    return;
 }
 
-function loadCSSFile(path) {    
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = path;
-    document.getElementsByTagName('head')[0].appendChild(link);
+brackets.loadCSSFile = (path) => { 
+    if (!path.endsWith(".css")) {
+        console.log("BracketsJS: " + path + " is not a css file.");
+        return;
+    } else {   
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = path;
+        document.getElementsByTagName('head')[0].appendChild(link);
+    }
 }
 
 // aliases
 var get = document.getElementById.bind(document); // aliases document.getElementById to just 'get'
 var delay = setTimeout.bind(window); // aliases setTimeout to just 'delay'
-const mult = (a, b) => a * b; // multiply
-const divid = (a, b) => a / b; // divide
-const add = (a, b) => a + b; // add
-const sub = (a, b) => a - b; // subtract
 const getAll = selector => document.querySelectorAll(selector); // quick alias to get all elements 
 const getRand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min; // get a random number
