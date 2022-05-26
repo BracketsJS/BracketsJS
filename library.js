6fb601c880113c15a06e438f46c9a150df8b8e1a
JaydenDev <willsjayden2@gmail.com>
2022-05-26 09:23:40 -0500

    Finished v1.2.0

--- library.js
+++ library.js
@@ -3,7 +3,7 @@
 // BracketsJS
 // Javascript libary with functions for live dom manipulation
 var brackets = {};
-const version = 'v1.2.0-releasePreview.2';
+const version = 'v1.2.0';
 
 console.log('%c BracketsJS: You are currently using ' + version, "color: green; font-weight: bold;");
 
