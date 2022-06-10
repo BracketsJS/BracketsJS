1184aba5dfee7dec16a51e5a836cdf8718495ef1
dinonull <dinonull@aol.com>
2022-06-10 17:18:33 -0400

    fix: make BracketsJS actually usable in the browser

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,27 @@
-const path = require("path");
-module.exports = {
-    entry: "./src/index.js",
-    mode: "production",
-    module: {
-        rules: [
-            {
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".js"],
-    },
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    optimization: {
-        minimize: true,
-        mangleExports: false,
-        minimizer: [
-            new TerserPlugin({
-                terserOptions: {
-                    keep_classnames: true,
-                    keep_fnames: true,
-                    mangle: false
-                }
-            })
-        ]
-    },
-    plugins: [
+const path = require('path');
 
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),  
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'brackets',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.js'],
+    modules: [path.resolve(__dirname, 'src')],
+  },
+  mode: 'development',
+  devtool: 'sourceMap',
 };
+
