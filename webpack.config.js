1479e8b89b9e11b74a0ad651246ed74261af094b
dinonull <dinonull@aol.com>
2022-06-13 14:08:28 -0400

    feat: allow BracketsJS to be accessed with $

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    library: 'brackets',
+    library: '$',
     libraryTarget: 'umd',
   },
   module: {
